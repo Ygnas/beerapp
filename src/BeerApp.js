01cbb44a87e9002024cbeb32c8bd9b2e18e6ad42
Ygnas <ygnas@outlook.com>
2022-10-31 14:09:07 +0000

    updated beerapp

--- src/BeerApp.js
+++ src/BeerApp.js
@@ -1,14 +1,44 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import Beer from './Beer'
+import { beerData } from './beerData'
+import Brewery from './Brewery';
 
 export default function BeerApp() {
-  return (
-    <>
-        <header>
-            <h1>The Random Beer App</h1>
-            <button>Show Another Beer</button>
-        </header>
-        <Beer/>
-    </>
-  )
+    const [beer, setBeer] = useState([]);
+    const [brewery, setBrewery] = useState([]);
+
+    // Called once
+    useEffect(() => {
+        randomBeer();
+    }, [])
+
+    // Called everytime brewery data changes.
+    useEffect(() => {
+        const URL = "https://api.openbrewerydb.org/breweries/" + brewery.brewery;
+        axios.get(URL)
+        .then((response) => {
+            setBrewery(response.data)
+        })
+    }, [brewery])
+
+    // Sets random brewery and beer 
+    function randomBeer() {
+        const randomBrewery = Math.floor(Math.random() * beerData.length);
+        const brewery = beerData[randomBrewery];
+        setBrewery(brewery);
+        const randomBeer = Math.floor(Math.random() * brewery.beers.length);
+        setBeer(brewery.beers[randomBeer]);
+    }
+
+    return (
+        <>
+            <header>
+                <h1>The Random Beer App</h1>
+                <button>Show Another Beer</button>
+            </header>
+            <Beer beer={beer}/>
+            <Brewery brewery={brewery}/>
+        </>
+    )
 }
