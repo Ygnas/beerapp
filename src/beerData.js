0420680660398163299e003daad7b9450e2b2106
Ygnas <ygnas@outlook.com>
2022-10-31 13:45:07 +0000

    sample beer data

---
+++ src/beerData.js
@@ -0,0 +1,168 @@
+export const beerData = [
+    {
+        beers: [
+            {
+                name: "Beamish",
+                description: "In 1792 Richard Beamish & Richard Crawford, two Cork merchants, established the famous Beamish & Crawford breweryID, situated in the heart of Cork city. To this day Beamish stout is being enjoyed in bars and homes throughout Ireland. Beamish has a rich roasted flavour with coffee and dark chocolate undertones, making it a genuine Irish stout.",
+                image: "https://beamish.ie/wp-content/themes/beamish//assets/images/beamish-pint.png",
+            }
+        ],
+        breweryID: "beamish-and-crawford-heineken-cork",
+    },
+    {
+        beers: [
+            {
+                name: "Murphys",
+                description: "Murphy's is an internationally recognized Irish stout, brewed since 1856 in the iconic Lady's Well Brewery, Cork. Classified as an Irish Dry Stout, Murphy's is dark in color and medium-bodied. It is silky smooth with toffee & coffee undertones, almost no bitterness, and a irresistible creamy finish.",
+                image: "https://www.murphys.com/wp-content/themes/murphys/assets/holding/pint-of-murphys-optimised.png",
+            }
+        ],
+        breweryID: "murphys-breweryID-heineken-cork"
+    },
+    {
+        beers: [
+            {
+                name: "REBEL RED",
+                description: "Our Rebel Red is proud to be different. It has been crafted to impart an exceptional Irish taste, creating a more accessible red ale that's distinctively different. It offers the drinker a pronounced caramel and biscuit finish, together with a balanced hop/malt profile and a full mouthfeel. It is perfect for sharing with friends and pairing with food. The Rebel is an exceptionally crafted Irish ale that adds colour to any conversation. Launched in 1998 Rebel Red was the top selling Exceptional Irish Red Ale in the market after 4 months in outlet activity delivering a huge sales uplift and winning a Gold Medal at the world beer award for Europe's Best Amber 2013.",
+                image: "https://www.franciscanwellbrewery.com/sites/franciscanwell/files/2021-07/Rebel%20Red.png"
+            },
+            {
+                name: "CHIEFTAIN IPA",
+                description: "Franciscan Well Chieftain IPA is a hugely popular IPA, and Franciscan Well's answer to Ireland's growing demand for IPA style beers. IPA's are traditionally high hopped to deliver intense bitterness that lingers on the palate which usually limits what you can drink or taste after it. With Chieftain IPA, Shane wants everything an IPA has to deliver, but he wants you to be able to enjoy another beer after it. Chieftain IPA is a great stepping stone into the land of big and bitter.",
+                image: "https://www.franciscanwellbrewery.com/sites/franciscanwell/files/2021-07/Chieftan%20IPA.png"
+            },
+            {
+                name: "FRIAR WEISSE",
+                description: "Franciscan Well Friar Weisse is a German style unfiltered wheat beer. It is brewed to be a fresh, zesty wheat beer with citrus notes on the palate and a powerful aroma of clove and banana imparted by a unique yeast strain. Shane adheres to the strict German brewing process to deliver this fantastic beer.",
+                image: "https://www.franciscanwellbrewery.com/sites/franciscanwell/files/2021-07/Friar%20Weisse.png"
+            },
+            {
+                name: "SHANDON STOUT",
+                description: "Franciscan Well Shandon Stout has a remarkably light style of it's own….Tall, dark and handsome, a drink that's deliciously different and an instant hit with indulgent food. With roasted rich flavours, choclately hints and a dry Irish character, it's a stout that's truly an attractive alternative.",
+                image: "https://www.franciscanwellbrewery.com/sites/franciscanwell/files/2021-07/Shandon%20Stout.png"
+            }
+        ],
+        breweryID: "the-franciscan-well-breweryID-cork"
+    },
+    {
+        beers: [
+            {
+                name: "Stag Kolsch",
+                description: "Stag Kolsch is a light, crisp, refreshing lager with a gentle hops and malt character, to be enjoyed by everyone. 4.2% ABV and only 36 kcal per 100ml (GF)",
+                image: "https://www.9whitedeer.ie/wp-content/uploads/2020/03/9-white-deer-product-img-8.jpg"
+            },
+            {
+                name: "Stag Bán",
+                description: "Stag Bán is  an easy going beer brewed at lower temperatures to create a cleaner profile, the malt profile is an uncomplicated design to give somebody to the beer without being heavy or cloying. 4.5% abv",
+                image: "https://www.9whitedeer.ie/wp-content/uploads/2020/03/9-white-deer-product-img-9.jpg"
+            },
+            {
+                name: "Stag IPA",
+                description: "A Classic IPA, using vast quantities of new age hops this IPA is full of flavour. Hop usage is late in the boil which gives big flavours and aroma without big bitterness. IPAs never serviced the gluten free market but that is now changed, and what a beer to change it with. 5% ABV",
+                image: "https://www.9whitedeer.ie/wp-content/uploads/2020/03/9-white-deer-product-img-10.jpg"
+            },
+            {
+                name: "Stag Stout",
+                description: "This is a double chocolate and Madagascan vanilla stout,  Stag stout is Ireland's first gluten free stout, based on the same recipe as our draft stout but with heaps of cacao nibs and vanilla but this version is bursting with chocolate. This is a rich and luxurious beer with incredible smoothness. 4.2% ABV",
+                image: "https://www.9whitedeer.ie/wp-content/uploads/2020/03/9-white-deer-product-img-11.jpg"
+            },
+            {
+                name: "Stag Rua",
+                description: "Stag Rua is a beer with big malt flavours and it's our impression of a perfect Irish Red Ale.  The body is moderate meaning it's not a heavy beer with an ABV of 4.2%. The flavour profile of Stag Rua is full of irresistible chocolate, caramel and toffee characteristics. These are derived from liberal use of chocolate and crystal malts beside the gorgeous Irish ale malt. No one ingredient takes over the beer, more of a depth of layers which reveal themselves slowly. 4.2%",
+                image: "https://www.9whitedeer.ie/wp-content/uploads/2020/03/9-white-deer-product-img-12.jpg"
+            },
+            {
+                name: "Black Lightning",
+                description: "This Black IPA is triple hopped with huge hop volumes of all the American hops expected of the style. The malt is in the back seat for this one, as there is enough dark malt to give colour and a limited roasty flavour with no malt astringency. Black Lightning is hopped from the mash to the fermentor. We use Willamette, Chinook, Cascade, Summit and Centennial hops in very large volumes, about 100% more than a regular beer, but we don't make it too bitter. Black Lightning is 56 IBU and the water profile is adjusted to make the hops seem more rounded rather than sharp and harsh which can easily happen when using so much hops. 6.5% ABV",
+                image: "https://www.9whitedeer.ie/wp-content/uploads/2020/03/9-white-deer-product-img-13.jpg"
+            }
+        ],
+        breweryID: "9-white-deer-breweryID-macroom"
+    },
+    {
+        beers: [
+            {
+                name: "KPA Kinsale Pale Ale",
+                description: "An American style Pale Ale bursting with hoppy tropical and juicy citrus fruit flavours, masterfully fused with biscuit and caramel malt aromas to produce a fantastic taste sensation. Medium in body with perfect carbonation, this beer will zip around your mouth reaching a beautiful clean finish.",
+                image: "https://www.blacksbrewery.com/wp-content/uploads/2020/08/kpa.png"
+            },
+            {
+                name: "Black IPA",
+                description: "This bad boy is a beer lover's dream come true. We have taken the amazing hoppiness of our IPAs, tuned it to perfection, and fused it together with the roasted malt backbone of a stout to create a hybrid beer that represents the best of both styles. Expect citrus, pine and berry notes to be suddenly ambushed by flavours of coffee, cocoa and liquorice. It's complex and creamy with low carbonation and an amazing dry finish.",
+                image: "https://www.blacksbrewery.com/wp-content/uploads/2020/08/black-ipa.png"
+            },
+            {
+                name: "1601",
+                description: "It's clean, crisp and refreshing, with a touch of freshly baked bread and honey aromas, followed by gentle floral and citrus notes due to the addition of Noble hops. Light and lively in the mouth, with a hint of hoppiness that will make your mouth water.",
+                image: "https://www.blacksbrewery.com/wp-content/uploads/2020/08/1601.png"
+            },
+            {
+                name: "Rocketship",
+                description: "This India Pale Ale hits you with a blast of hop aromas in the nose like passion fruit and lemon, that gradually develops into pineapple and orange split ice lolly notes. Flavours of candied fruit, grapefruit and orange peel add to this interplanetary taste adventure, which culminates in a bitter hoppy refreshing finish.",
+                image: "https://www.blacksbrewery.com/wp-content/uploads/2020/08/rocketship-ipa.png"
+            },
+            {
+                name: "World's End",
+                description: "A rich, dark, luxurious Imperial Stout brewed with Fairtrade cacao husks and Madagascan vanilla pods. It's like an adventure for you tastebuds…. straight off the bat waves of chocolate, lactose, caramel and dried fruit will assault your senses. Next up is a touch of smoke and liquorice blended with coconut and coffee. The mouth feel is full and creamy, balanced fantastically with a berry like acidity that keeps this beer juicy and super drinkable. Suitable for ageing, but try to drink it before the end of the world!",
+                image: "https://www.blacksbrewery.com/wp-content/uploads/2020/08/worlds-end.png"
+            },
+            {
+                name: "Marching Powder",
+                description: "A super modern Cryo IPA with amazing drinkability & an explosion of Simcoe & Mosaic hops from start to finish. In the nose tropical & citrus fruit supported by floral & piney aromas. On the palate, bold, fruity & zesty flavours with a balanced refreshing hoppy bitterness & a fantastic thirst quenching finish.",
+                image: "https://www.blacksbrewery.com/wp-content/uploads/2020/08/marching-powder.png"
+            }
+        ],
+        breweryID: "blacks-breweryID-kinsale"
+    },
+    {
+        beers: [
+            {
+                name: "Indian Summer Beer",
+                description: "Our hybrid beer, this rare beauty captures the best of ale and lager. The finest Irish lager malt, crystal malt and ale yeast are the secret to her svelte body. Delicately bittered using a unique combination of three new world hops, a citrus aroma gives Indian Summer the sweetest summer glow",
+                image: "https://www.cottonball.ie/wp-content/uploads/2020/04/India.png"
+            },
+            {
+                name: "Kerry Lane Pale Ale",
+                description: "Like any Cork and Kerry marriage this pale ale is bitter sweet! Each pour throws a full-bodied, bitter sweet duel at your tastebuds. A classic aromaic hop flavour is acentuated by a late kettle hop addition. This is followed by warm maturation using both a traditional hop and one of the new hop breeds .",
+                image: "https://www.cottonball.ie/wp-content/uploads/2020/04/kerry.png"
+            },
+            {
+                name: "Lynch's Hand Crafted Stout",
+                description: "Hand crafted from 5 malts and traditional flaked barley. Moderately bittered and late hopped with US and New Zealand hops. This stout is as dark as our great grandfather Humphrey's black humour and has a warm and mature flavour. With a phenomenal marriage of coffee roast, caramel lush, balanced by a clean bitterness exploding into a tangerine, mandarin aromatic delivery.",
+                image: "https://www.cottonball.ie/wp-content/uploads/2020/04/stout.png"
+            },
+            {
+                name: "Mayfield 5 Lager",
+                description: "This Pilsner Lager, like the Noble Northsider's adventures, spans the Atlantic, brewed using 100% Irish malted barley, clean bittered with three U.S. grown hops followed by a late kettle addition of Noble Hops. This Beer has charachter. Each pour is clean and refreshing with a subtle aromatic hop flavour arising from a bed of light caramel malt",
+                image: "https://www.cottonball.ie/wp-content/uploads/2020/04/MayfieldLager.png"
+            }
+        ],
+        breweryID: "cotton-ball-brewing-company-mayfield"
+    },
+    {
+        beers: [
+            {
+                name: "Seisiún - Easy Going IPA",
+                description: "Lots of flavour and low alcohol make this a perfect beer to serve over a period of time at a party, relaxed gathering or - as we've tested! - beer festival. An ideal pairing is pizza, particularly anything that includes the fabulous chorizo or salami from Gubbeen Smokehouse, as the beer crisply cuts through all that meaty, cheesy gorgeousness with each sip. On the lighter side, try this Seisiún IPA with a St Tola goat cheese and fresh beetroot salad, tossed in a ginger and lime dressing, and see how the hop bitterness complements the citrus.",
+                image: "https://www.eightdegrees.ie/wordpress/wp-content/uploads/2020/11/beer-seisiun.jpg"
+            },
+            {
+                name: "Sunburnt - Irish Red Ale",
+                description: "Serve with a cheese and charcuterie platter: there are enough hops here to stand up to sharp Hegarty's cheddar cheese and cut through the richness of Gubbeen chorizo. Buttery, rich cheese, whether it has a bloomy rind - eg Cooleeney Farmhouse Cheese, Wicklow Baun - or a washed rind like Cais Rua work well here, the carbonation in the ale effectively cleaning the palate between each bite. Lamb - sweet and ever so slightly fatty - is also a good pairing. Match with a traditional Irish stew or make some lamb burgers, with lots of rosemary, and let the Sunburnt Irish Red shine through.",
+                image: "https://www.eightdegrees.ie/wordpress/wp-content/uploads/2020/11/beer-sunburnt.jpg"
+            },
+            {
+                name: "Howling Gale - Irish Pale Ale",
+                description: "Pale Ales are traditionally seen a great foil for spicy food - there's a little bit of sweetness there that will cut the chilli burn - but don't miss the chance to have Howling Gale with fish and chips (or in the batter for the fish!). It's well worth trying with a Cashel Blue and Broccoli Gratin, the bright citrus notes act like a squeeze of lemon with smoked salmon or pair it with semi-soft, washed rind cheeses like Milleens, Durrus and Gubbeen.",
+                image: "https://www.eightdegrees.ie/wordpress/wp-content/uploads/2020/11/beer-howling-gale.jpg",
+            },
+            {
+                name: "Bohemian - Pilsner Lager",
+                description: "this is the perfect beer to have with pizza, working brilliantly with the tomato sauce and cutting through all that melted cheese. The snappy bitterness is a delightful counterpoint to the sweetness of shellfish - try it as an aperitif, served in champagne flutes, with a half-dozen Irish oysters or even have a glass with some savoury home-popped popcorn.",
+                image: "https://www.eightdegrees.ie/wordpress/wp-content/uploads/2020/11/beer-bohemian.jpg"
+            }
+        ],
+        breweryID: "eight-degrees-brewing-mitchelstown"
+    },
+
+]
