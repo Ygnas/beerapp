970d2535e05c989717172fb9125e8eab9a97cbf2
Ygnas <ygnas@outlook.com>
2022-10-31 14:08:28 +0000

    Simple tooltip display

--- src/Brewery.js
+++ src/Brewery.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import ReactTooltip from 'react-tooltip'
 
 export default function Brewery( {brewery} ) {
   return (
-    <div>Brewery</div>
+    <section>
+        <p data-tip data-for="breweryTooltip">{brewery.name}</p>
+        <ReactTooltip delayUpdate={1000} clickable="true" id="breweryTooltip" place="top" effect="solid">
+            <p>Brewery Type: {brewery.brewery_type}</p>
+            <p>City: {brewery.county_province}</p>
+            <p>Country: {brewery.country}</p>
+            <p>Phone: {brewery.phone}</p>
+            <p>Website: {brewery.website_url}</p>
+        </ReactTooltip>
+    </section>
   )
 }
