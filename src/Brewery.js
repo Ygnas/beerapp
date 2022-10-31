68eeebb45f925e14b8e9b382ca936c8223c1b074
Ygnas <ygnas@outlook.com>
2022-10-31 14:23:24 +0000

    fixed clickable={true}

--- src/Brewery.js
+++ src/Brewery.js
@@ -5,7 +5,7 @@ export default function Brewery( {brewery} ) {
   return (
     <section>
         <p data-tip data-for="breweryTooltip">{brewery.name}</p>
-        <ReactTooltip delayUpdate={1000} clickable="true" id="breweryTooltip" place="top" effect="solid">
+        <ReactTooltip delayUpdate={1000} clickable={true} id="breweryTooltip" place="top" effect="solid">
             <p>Brewery Type: {brewery.brewery_type}</p>
             <p>City: {brewery.county_province}</p>
             <p>Country: {brewery.country}</p>
